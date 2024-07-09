ad1e095480c3e2aed7d4161334db4becbb168088
Maria Morales <victorina23x@gmail.com>
2024-07-09 11:35:32 -0400

    Added getHumanChoice function. Prompt userfor choice (rock, paper, or scissors) and return the value. Handle invalid or incorrect input

--- javaScript.js
+++ javaScript.js
@@ -1,4 +1,4 @@
-function getComputerChoice(){
+ /* function getComputerChoice(){
     let rock = "Rock";
     let paper = "Paper";
     let scissors = "Scissors";
@@ -13,6 +13,26 @@ function getComputerChoice(){
     else {
         return scissors;
     }
-}
+}  
 
 console.log(getComputerChoice());
+*/
+
+function getHumanChoice(){
+    let humanChoice = prompt("Do you have rock, paper or scissors? ");
+
+    if(humanChoice.toLowerCase() === "rock"){
+        return "Rock";
+    }
+    else if(humanChoice.toLocaleLowerCase() === "paper"){
+        return "Paper";
+    }
+    else if(humanChoice.toLocaleLowerCase() === "scissors"){
+        return "Scissors";
+    }else{
+        return "That option is not valid or you wrote it wrong";
+    }
+
+}
+
+console.log(getHumanChoice());
